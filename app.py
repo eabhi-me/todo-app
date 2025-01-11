5fc278bccd88f2e4de0d628fa70aba7069cb9558
eabhi-me <abhiydv423@gmail.com>
2025-01-11 22:22:19 +0530

    set production server

--- app.py
+++ app.py
@@ -76,5 +76,13 @@ def serach_by_key():
     return redirect(url_for('home'))
 
 # development server debug mmode on
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+# deployment server
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
