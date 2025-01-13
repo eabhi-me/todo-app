e1439458340b5021b5baeea225106f5bc51d828d
eabhi-me <abhiydv423@gmail.com>
2025-01-13 19:47:48 +0530

    deployment server

--- app.py
+++ app.py
@@ -2,13 +2,13 @@
 from app import app
 
 # running the development server
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
 
 
-# # deployment server
-# import os
+# deployment server
+import os
 
-# if __name__ == "__main__":
-#     port = int(os.environ.get("PORT", 5000))
-#     app.run(host="0.0.0.0", port=port)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
