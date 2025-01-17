0743460c9b3ebffc588f7f011fa70b3d0984cd0c
23f3002662 <23f3002662@ds.study.iitm.ac.in>
2025-01-18 00:14:38 +0530

    minor change in ui

--- app.py
+++ app.py
@@ -2,12 +2,12 @@
 from app import app
 
 # running the development server
-# if __name__ == '__main__':
-#     app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
 
 
 # deployment server
-import os
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+# import os
+# if __name__ == "__main__":
+#     port = int(os.environ.get("PORT", 5000))
+#     app.run(host="0.0.0.0", port=port)
